29cfdee0b362d19ab55f241bc28dc5aa7d311f4b
Woodyrom <dcw13a@acu.edu>
2016-02-24 11:45:06 -0600

    Added files via upload

---
+++ ProjectEdited/coverpage/index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--
+Template Name: News Magazine
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+
+<?php
+
+/**************************
+*
+* Database Connections
+*
+***************************/
+$link = new mysqli("localhost","root","","user_db");
+
+
+if ($link->connect_errno) {
+    printf("Connect failed: %s\n", $link->connect_error);
+    exit();
+}
+/**************************
+*
+* Database interactions
+*
+***************************/
+$loggedin = true;
+if(isset($_COOKIE["AppName"]))
+{
+	$name = $_COOKIE["AppName"];
+	$cryptedCookie = $_COOKIE[$name];
+	$cryptedName = crypt($name,"ilovetacos");
+	if($cryptedCookie == $cryptedName)
+		$loggedin = true;
+}
+else
+	$action = "none";
+
+if(isset($_REQUEST["action"]))
+	$action = $_REQUEST["action"];
+else
+	$action = "none";
+
+$message = "";
+?>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>ACU Times</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+<script type="text/javascript" src="layout/scripts/jquery.min.js"></script>
+<!-- Homepage Specific -->
+<script type="text/javascript" src="layout/scripts/galleryviewthemes/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="layout/scripts/galleryviewthemes/jquery.timers.1.2.js"></script>
+<script type="text/javascript" src="layout/scripts/galleryviewthemes/jquery.galleryview.2.1.1.min.js"></script>
+<script type="text/javascript" src="layout/scripts/galleryviewthemes/jquery.galleryview.setup.js"></script>
+<!-- / Homepage Specific -->
+</head>
+<body id="top">
+<div class="wrapper col0">
+  <div id="topline">
+    <?php
+		if($loggedin){
+			print "Welcome, ". $_COOKIE["AppName"];
+			print "!";
+		}
+		else
+			print "Not logged in.";
+	?>
+		<ul>
+			<li><?php echo "<a href='../index.php'>    Logout</a>" ?></li>
+		</ul>
+        
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="header">
+    <div class="fl_left">
+      <h1><a href="index.html"><strong>A</strong>CU <strong>T</strong>imes</a></h1>
+      <p>Yesterdays News, Today!</p>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col2">
+  <div id="topbar">
+    <div id="topnav">
+      <ul>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="pages/style-demo.html">About </a></li>
+        <li><a href="pages/full-width.html">News</a></li>
+        <li><a href="#">DropDown</a>
+          <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+          </ul>
+        </li>
+        <li class="last"><a href="#">A Long Link Text</a></li>
+      </ul>
+    </div>
+    <div id="search">
+      <form action="#" method="post">
+        <fieldset>
+          <legend>Site Search</legend>
+          <input type="text" value="Search Our Website&hellip;"  onfocus="this.value=(this.value=='Search Our Website&hellip;')? '' : this.value ;" />
+          <input type="submit" name="go" id="go" value="Search" />
+        </fieldset>
+      </form>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div class="container">
+    <div class="content">
+      <div id="featured_slide">
+        <ul id="featurednews">
+          <li><img src="images/acu_stadium.jpg" alt="" />
+            <div class="panel-overlay">
+              <h2>Nullamlacus dui ipsum</h2>
+              <p>Temperinte interdum sempus odio urna eget curabitur semper convallis nunc laoreet.<br />
+                <a href="#">Continue Reading &raquo;</a></p>
+            </div>
+          </li>
+          <li><img src="images/acu_campus.jpg" alt="" />
+            <div class="panel-overlay">
+              <h2>Aliquatjusto quisque nam</h2>
+              <p>Temperinte interdum sempus odio urna eget curabitur semper convallis nunc laoreet.<br />
+                <a href="#">Continue Reading &raquo;</a></p>
+            </div>
+          </li>
+          <li><img src="images/gwaves.jpg" alt="" />
+            <div class="panel-overlay">
+              <h2>Dapiensociis temper donec</h2>
+              <p>Temperinte interdum sempus odio urna eget curabitur semper convallis nunc laoreet.<br />
+                <a href="#">Continue Reading &raquo;</a></p>
+            </div>
+          </li>
+          <li><img src="images/acu.jpg" alt="" />
+            <div class="panel-overlay">
+              <h2>Semvelit nonummy odio tempus</h2>
+              <p>Justolacus eger at pede felit in dictum sempus elit curabitur ipsum. Ametpellentum.<br />
+                <a href="#">Continue Reading &raquo;</a></p>
+            </div>
+          </li>
+          <li><img src="images/abroad.jpg" alt="" />
+            <div class="panel-overlay">
+              <h2>Pedefamet orci orci quisque</h2>
+              <p>Nonnam aenenasce morbi liberos malesuada risus id donec volutpat estibulum curabitae.<br />
+                <a href="#">Continue Reading &raquo;</a></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="column">
+      <ul class="latestnews">
+        <li><img src="images/stadium_100X100.jpg" alt="" />
+          <p><strong><a href="/Users/Chance/Documents/School/WebDev2/coverpage/pages/article1.html">Indonectetus facilis leo.</a></strong> Nullamlacus dui ipsum cons eque loborttis non euis que morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque.</p>
+        </li>
+        <li><img src="images/admin_100X100.jpg" alt="" />
+          <p><strong><a href="#">Indonectetus facilis leo.</a></strong> Nullamlacus dui ipsum cons eque loborttis non euis que morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque.</p>
+        </li>
+        <li class="last"><img src="images/gwaves_100X100.jpg" alt="" />
+          <p><strong><a href="#">Indonectetus facilis leo.</a></strong> Nullamlacus dui ipsum cons eque loborttis non euis que morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque.</p>
+        </li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="adblock">
+    <br class="clear" />
+  </div>
+  <div id="hpage_cats">
+    <div class="fl_left">
+      <h2><a href="#">Environment &raquo;</a></h2>
+      <img src="images/acu_logo.png" alt="" />
+      <p><strong><a href="#">Yo, Stuff Happened.</a></strong></p>
+      <p>This is a test article for ACU Times. Lorem ipsum dolor sit amet, qui ne suas labitur alterum. Mel fabulas dolores efficiantur et, mea novum invidunt lobortis cu. Pri an ridens principes scripserit. Qui et molestie appellantur. Eu qui nonumy graeci audire.
+</p>
+    </div>
+    <div class="fl_right">
+      <h2><a href="#">Technology &raquo;</a></h2>
+      <img src="images/acu_logo.png" alt="" />
+      <p><strong><a href="#">Articles!</a></strong></p>
+      <p>This is another cool test article for ACU Times. Lorem ipsum dolor sit amet, qui ne suas labitur alterum. Mel fabulas dolores efficiantur et, mea novum invidunt lobortis cu. Pri an ridens principes scripserit. Qui et molestie appellantur. Eu qui nonumy graeci audire.
+</p>
+    </div>
+    <br class="clear" />
+    <div class="fl_left">
+      <h2><a href="#">Entertainment &raquo;</a></h2>
+      <img src="images/acu_logo.png" alt="" />
+      <p><strong><a href="#">Articles!</a></strong></p>
+      <p>This is another cool test article for ACU Times. Lorem ipsum dolor sit amet, qui ne suas labitur alterum. Mel fabulas dolores efficiantur et, mea novum invidunt lobortis cu. Pri an ridens principes scripserit. Qui et molestie appellantur. Eu qui nonumy graeci audire.
+</p>
+    </div>
+    <div class="fl_right">
+      <h2><a href="#">Politics &raquo;</a></h2>
+      <img src="images/acu_logo.png" alt="" />
+      <p><strong><a href="#">Articles!</a></strong></p>
+      <p>This is another cool test article for ACU Times. Lorem ipsum dolor sit amet, qui ne suas labitur alterum. Mel fabulas dolores efficiantur et, mea novum invidunt lobortis cu. Pri an ridens principes scripserit. Qui et molestie appellantur. Eu qui nonumy graeci audire.
+</p>
+	</div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div class="container">
+    <div class="content">
+      <div id="hpage_latest">
+        <h2>Feugiatrutrum rhoncus semper</h2>
+        <ul>
+          <li><img src="images/acu_campus2.jpg" alt="" />
+            <p>Nullamlacus dui ipsum conseqlo borttis non euisque morbipen a sdapibulum orna.</p>
+            <p>Urnau ltrices quis curabitur pha sellent esque congue magnisve stib ulum quismodo nulla et.</p>
+            <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+          </li>
+          <li><img src="images/acu_campus2.jpg" alt="" />
+            <p>Nullamlacus dui ipsum conseqlo borttis non euisque morbipen a sdapibulum orna.</p>
+            <p>Urnau ltrices quis curabitur pha sellent esque congue magnisve stib ulum quismodo nulla et.</p>
+            <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+          </li>
+          <li class="last"><img src="images/acu_campus2.jpg" alt="" />
+            <p>Nullamlacus dui ipsum conseqlo borttis non euisque morbipen a sdapibulum orna.</p>
+            <p>Urnau ltrices quis curabitur pha sellent esque congue magnisve stib ulum quismodo nulla et.</p>
+            <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+          </li>
+        </ul>
+        <br class="clear" />
+      </div>
+    </div>
+    <div class="column">
+      <div class="holder"><a href="#"><img src="images/wpac_sm.jpg" alt="" /></a></div>
+	</div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="footer">
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox last">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper">
+  <div id="socialise">
+    <ul>
+      <li><a href="#"><img src="layout/images/facebook.gif" alt="" /><span>Facebook</span></a></li>
+      <li><a href="#"><img src="layout/images/rss.gif" alt="" /><span>FeedBurner</span></a></li>
+      <li class="last"><a href="#"><img src="layout/images/twitter.gif" alt="" /><span>Twitter</span></a></li>
+    </ul>
+    <div id="newsletter">
+      <h2>NewsLetter Sign Up !</h2>
+      <p>Please enter your Email and Name to join.</p>
+      <form action="#" method="post">
+        <fieldset>
+          <legend>Digital Newsletter</legend>
+          <div class="fl_left">
+            <input type="text" value="Enter name here&hellip;"  onfocus="this.value=(this.value=='Enter name here&hellip;')? '' : this.value ;" />
+            <input type="text" value="Enter email address&hellip;"  onfocus="this.value=(this.value=='Enter email address&hellip;')? '' : this.value ;" />
+          </div>
+          <div class="fl_right">
+            <input type="submit" name="newsletter_go" id="newsletter_go" value="&raquo;" />
+          </div>
+        </fieldset>
+      </form>
+      <p>To unsubsribe please <a href="#">click here &raquo;</a>.</p>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col8">
+  <div id="copyright">
+    <p class="fl_left">Copyright &copy; 2014 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <br class="clear" />
+  </div>
+</div>
+</body>
+</html>
